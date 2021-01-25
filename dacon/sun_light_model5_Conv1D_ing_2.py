befdf33aee5a95acb78e3f910b290a8536d615f8
TaeYeon <lovehjty@gmail.com>
2021-01-26 02:14:28 +0900

    2021.01.26

--- dacon/sun_light_model5_Conv1D_ing_2.py
+++ dacon/sun_light_model5_Conv1D_ing_2.py
@@ -15,20 +15,49 @@ from tensorflow.keras.optimizers import RMSprop, SGD, Nadam
 #0. 함수정의
 train = pd.read_csv('./dacon/data/train/train.csv')
 submission = pd.read_csv('./dacon/data/sample_submission.csv')
-# def Add_features(data):
-#   c = 243.12
-#   b = 17.62
-#   gamma = (b * (data['T']) / (c + (data['T']))) + np.log(data['RH'] / 100)
-#   dp = ( c * gamma) / (b - gamma)
-#   data.insert(1,'Td',dp)
-#   data.insert(1,'T-Td',data['T']-data['Td'])
-#   data.insert(1,'GHI',data['DNI']+data['DHI'])
-#   return data
+
+# Hour - 시간
+# Minute - 분
+# DHI - 수평면 산란일사량(Diffuse Horizontal Irradiance (W/m2))
+# DNI - 직달일사량(Direct Normal Irradiance (W/m2))
+# WS - 풍속(Wind Speed (m/s))
+# RH - 상대습도(Relative Humidity (%))
+# T - 기온(Temperature (Degree C))
+# Target - 태양광 발전량 (kW)
+
+#pred데이터 병합
+def preprocess_data2(data):
+    temp = data.copy()
+    return temp.iloc[-48:, :]
+
+df_test = []
+
+for i in range(81):
+    file_path = './dacon/data/test/' + str(i) + '.csv' #test파일 불러오기
+    temp = pd.read_csv(file_path) #pandas로 파일 읽기
+    temp = preprocess_data2(temp) 
+    df_test.append(temp)
+
+X_test = pd.concat(df_test)
+#Attach padding dummy time series
+X_test = X_test.append(X_test[-96:])
+df_test  = X_test.drop(['Day','Minute'],axis=1)
+#print(df_test)#[3984 rows x 7 columns]
+
+#x_train
+# GHI 정립 전위모델 
+def Add_features(data):
+    data['cos'] = np.cos(np.pi/2 - np.abs(data['Hour']%12 - 6)/6*np.pi/2)
+    data.insert(1,'GHI',data['DNI']*data['cos']+data['DHI'])
+    data.drop(['cos'], axis= 1, inplace = True)
+    return data
+
+train = Add_features(train)
 
 def preprocess_data(data, is_train=True):
     
     temp = data.copy()
-    temp = temp[['Hour', 'TARGET', 'DHI', 'DNI', 'WS', 'RH', 'T']] # 컬럼 추출
+    temp = temp[['Hour', 'TARGET', 'GHI', 'DHI', 'DNI', 'WS', 'RH', 'T']] # 컬럼 추출
 
     if is_train==True:          
     
@@ -36,11 +65,11 @@ def preprocess_data(data, is_train=True):
         temp['Target2'] = temp['TARGET'].shift(-48*2).fillna(method='ffill') #  -48시간을 열2개 추가하여 채움  +2일 타겟 데이터
         temp = temp.dropna() #결측값 제거
         
-        return temp.iloc[:-96] #예측날짜 제거 (-2일)
+        return temp.iloc[:-96] #예측날짜 제거 (-2일) 마지막 2일이 빈다.
 
     elif is_train==False:
         
-        temp = temp[['Hour', 'TARGET', 'DHI', 'DNI', 'WS', 'RH', 'T']] 
+        temp = temp[['Hour', 'TARGET', 'GHI', 'DHI', 'DNI', 'WS', 'RH', 'T']] # 컬럼 추출
                                
         return temp.iloc[-48:, :] # if문 진행 -48~끝까지, 뒤에서 6일째 데이터만 남김 - if 데이터 +1~2일 예측 ex_ 1일 - 2, 3일 예측 / 2일 - 3,4일 예측
 
@@ -48,36 +77,28 @@ df_train = preprocess_data(train)
 df_train.iloc[:48] # T1, T2추가된 df_train 0~48까지 자르기
 
 #============================================
-#test데이터 병합
-df_test = []
-
-for i in range(81):
-    file_path = './dacon/data/test/' + str(i) + '.csv' #test파일 불러오기
-    temp = pd.read_csv(file_path) #pandas로 파일 읽기
-    temp = preprocess_data(temp, is_train=False) 
-    df_test.append(temp)
-
-#numpy 변환
-x_pred = pd.concat(df_test)
-x_pred = x_pred.to_numpy()
-#============================================
-
 #numpy 변환
 x_train = preprocess_data(df_train) #뒤에 두개 제외 Y값
 x_train = x_train.to_numpy()
 
+#numpy 변환
+x_pred = pd.concat(df_test)
+x_pred = x_pred.to_numpy()
+print(x_train)
+print(x_pred)
+#============================================
+'''
+# MinMax
+# scaler = MinMaxScaler()
+# scaler.fit(x_train[:,:-2])
+# x_train[:,:-2] = scaler.transform(x_train[:,:-2])
+# x_pred = scaler.transform(x_pred)
 
-# #standar
-scaler = MinMaxScaler()
+#Standar
+scaler = StandardScaler()
 scaler.fit(x_train[:,:-2])
 x_train[:,:-2] = scaler.transform(x_train[:,:-2])
 x_pred = scaler.transform(x_pred)
-
-# scaler1 = StandardScaler() #개구려.... 데이터 엉망으로 나오잖아 ㅜㅜ 이데이터엔 넌 아닌것같아 미안해 다음에만나
-# scaler1.fit(x_pred)
-# x_pred = scaler1.transform(x_pred)
-# print(x_train.shape)
-# print(x_pred.shape)
 
 #데이터 분할
 def split_xy(dataset, time_steps):
@@ -119,7 +140,7 @@ x_pred = split_x(x_pred,1)
 # 288     0     0.0    0    0  0.8  80.92  -2.8
 
 #============================================
-'''
+
 #train_sprit_t
 x_train, x_test, y1_train, y1_test = train_test_split(x, y1, train_size = 0.8, shuffle = True, random_state = 0)
 x_train, x_val, y1_train, y1_val = train_test_split(x_train, y1_train, train_size = 0.8, shuffle = True, random_state = 0)

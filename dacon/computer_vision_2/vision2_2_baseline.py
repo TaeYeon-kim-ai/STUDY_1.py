ed893f88336037d5baa7d3cc080c2fbb19b5fc20
tykim <lovehjty@gmail.com>
2021-02-15 23:34:17 +0900

    2021.02.15

--- dacon/computer_vision_2/vision2_2_baseline.py
+++ dacon/computer_vision_2/vision2_2_baseline.py
@@ -68,8 +68,8 @@ transforms_test = transforms.Compose([
 ])
 
 
-trainset = MnistDataset("C:/STUDY/dacon/computer_vision_2/dirty_mnist_2nd_answer.csv", transforms_train)
-testset = MnistDataset("C:/STUDY/dacon/computer_vision_2/sample_submission.csv", transforms_test)
+trainset = MnistDataset("C:/data/vision_2/dirty_mnist_2nd_noise_clean","C:/data/vision_2/dirty_mnist_2nd_answer.csv", transforms_train)
+testset = MnistDataset("C:/data/vision_2/test_dirty_mnist_2nd_noise_clean","C:/data/vision_2/sample_submission.csv", transforms_test)
 
 train_loader = DataLoader(trainset, batch_size=256, num_workers=8)
 test_loader = DataLoader(testset, batch_size=32, num_workers=4)
@@ -91,7 +91,6 @@ class MnistModel(nn.Module):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = MnistModel().to(device)
 print(summary(model, input_size=(1, 3, 256, 256), verbose=0))
-
 
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 criterion = nn.MultiLabelSoftMarginLoss()
@@ -117,24 +116,33 @@ for epoch in range(num_epochs):
             acc = (outputs == targets).float().mean()
             print(f'{epoch}: {loss.item():.5f}, {acc.item():.5f}')
 
-
-submit = pd.read_csv("C:/STUDY/dacon/computer_vision_2/sample_submission.csv")
-
-model.eval()
-batch_size = test_loader.batch_size
-batch_index = 0
-for i, (images, targets) in enumerate(test_loader):
-    images = images.to(device)
-    targets = targets.to(device)
-    outputs = model(images)
-    outputs = outputs > 0.5
-    batch_index = i * batch_size
-    submit.iloc[batch_index:batch_index+batch_size, 1:] = \
-        outputs.long().squeeze(0).detach().cpu().numpy()
+if __name__ == '__main__':
     
-submit.to_csv("C:/STUDY/dacon/computer_vision_2/submission.csv", index=False)
+    # 평가 폴더를 열음
+    submit = pd.read_csv("C:/data/vision_2/sample_submission.csv")
 
+    # 이벨류 모드로 전환
+    model.eval()
 
+    # 베치사이즈는 테스트로더 베치사이즈
+    batch_size = test_loader.batch_size
+    # 인덱스 0부터 시작
+    batch_index = 0
+    # 이벨류 모드를 테스트 셋으로 진행하고 파일에 입력함
+    for i, (images, targets) in enumerate(test_loader):
+        images = images.to(device)
+        targets = targets.to(device)
+        outputs = model(images)
+        outputs = outputs > 0.5
+        batch_index = i * batch_size
+        submit.iloc[batch_index:batch_index+batch_size, 1:] = \
+            outputs.long().squeeze(0).detach().cpu().numpy()
+
+    # 저장함
+    submit.to_csv("C:/data/vision_2/submission.csv", index=False)
+
+    del images
+    del targets
 
 print("제발 돌아가라 ㅜㅜ 잠좀 자자!!!!!")
 print("제발 돌아가라 ㅜㅜ 잠좀 자자!!!!!")

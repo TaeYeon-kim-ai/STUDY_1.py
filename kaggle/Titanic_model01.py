9b81d8bc2b3f68c084abc2b157ea734e037cd569
KTY <76669300+TaeYeon-kim-ai@users.noreply.github.com>
2021-04-23 23:16:23 +0900

    Add files via upload

---
+++ kaggle/Titanic_model01.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+
+#1. DATA정보정리
+# pclass : passenger Class 승객등급
+# survuved : 생존여부 (생존1, 사망0)
+# name : 이름
+# sex : 성별
+# sibsp : 동승한 형제 또는 배우자 수
+# parch : 동승한 보모 또는 자녀 수
+# ticket : 티켓 번호
+# fare : 승객 지불 요금
+# cabin : 선실 이름
+# embarked : 승선항(c = 쉘부르크, Q= 퀸즈타운, S=사우스 햄튼)
+# body : 사망자 확인 번호
+# bome.dest : 고향/목적지
+
+
+
+
+
+
+
